ca25d0da84024103ebca8c4184bdd6139e0c10b9
yeonoel <yeopevroguinoel@gmail.com>
2022-12-10 20:49:10 +0000

    pos map

--- db.config.js
+++ db.config.js
@@ -3,7 +3,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize =  new Sequelize(
 'pos_map_db',
  'root',
-'******',
+'Le47492156@',
 {
   dialect : 'mysql',
   'host' : 'localhost'
